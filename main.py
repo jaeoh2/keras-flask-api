07dda42caa8c2aad157402982834c79a79f5535e
jaeoh2 <suby38@gmail.com>
2017-06-21 00:19:21 +0900

    Train mnist model, Update TF version

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, request
+from skimage.io import imread, imsave
+from skimage.transform import resize
+import numpy as np
+from keras.models import model_from_json
+import tensorflow as tf
+import re
+import sys
+import os
+import base64
 
 app = Flask(__name__)
 
+def model_init():
+    with open("model.json","r") as json_file:
+        loaded_model_json = json_file.read()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("model.h5")
+    print("Loaded Model from disk")
+    
+    loaded_model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=['accuracy'])
+    graph = tf.get_default_graph()
+    
+    return loaded_model, graph
+
+def convertImage(imgData1):
+    imgstr = re.search(r'base64,(.*)', imgData1).group(1)
+    with open('output.png', 'wb') as output:
+        output.write(imgstr.decode('base64'))
+
+model, graph = model_init()
+        
 @app.route('/')
 #def hello_world():
 #    return "Deep learning based lung cancer analyzer Flask API repository"
@@ -19,9 +47,23 @@ def add_numbers():
 #    return "Hell World!"
 def predict():
     imgData = request.get_data()
-    print("debug") 
-    return "predict"
-
+    convertImage(imgData)
+    print("debug")
+    
+    x = imread('output.png', as_grey=True)
+    x = np.invert(x.astype(np.uint8))
+    x = resize(x,(28,28))
+    x = x.reshape(1,28,28,1)
+    print("debug2")
+    
+    with graph.as_default():
+        out = model.predict(x)
+        print(out)
+        print(np.argmax(out, axis=1))
+        print("debug3")
+        
+        response = np.array_str(np.argmax(out, axis=1))
+        return response
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=5000, debug=True)
